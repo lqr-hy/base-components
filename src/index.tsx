b4efdd2ae5135e81a531275782a6c0228f472189
lqr <1278297890@qq.com>
2022-10-16 23:37:24 +0800

    feat: 打包

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-import './styles/index.scss'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { default as Button } from "./components/Button/Button";
+export { ColorPick } from './components/ColorPick';
+export { default as Icon } from './components/Icon/Icon';
+export { default as Transition } from './components/Transition/Transition';
+export { Menu, MenuItem, SubMenu } from './components/Menu'
+export { default as LongListScroll} from './components/LongListScroll'
