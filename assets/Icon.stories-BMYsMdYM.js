312bdb54fad2aa5c15cb373b239272d1f436a5fe
lqr-hy <lqr-hy@users.noreply.github.com>
2024-08-10 01:59:49 +0000

    deploy: a8a8545208e098c1d67ec5a8e8ed86a5f647cedf

--- assets/Icon.stories-BMYsMdYM.js
+++
@@ -1,6 +0,0 @@
-import{I as a}from"./index-Bmv8Ovwm.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./index-rNTiGNI1.js";import"./inheritsLoose-Co2FXOuK.js";import"./index-D16Yfzz8.js";import"./client-ngufn95y.js";const d={title:"Component/Icon",component:a,tags:["autodocs"]},o={args:{icon:"coffee",theme:"primary"}};var r,t,e;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    icon: ('coffee' as const),
-    theme: ('primary' as const)
-  }
-}`,...(e=(t=o.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,d as default};
---
+++ assets/LongListScroll.stories-CtjVh66j.js
@@ -0,0 +1,8 @@
+import{j as l}from"./jsx-runtime-DEdD30eg.js";import{L as c}from"./index-zmJbRG5A.js";import"./index-RYns6xqu.js";import"./index-rNTiGNI1.js";import"./inheritsLoose-Co2FXOuK.js";import"./index-D16Yfzz8.js";import"./client-ngufn95y.js";const n=[];for(let t=0;t<100;t++)n.push({name:t,num:t+"你好"});const d=(t,a)=>a.map((i,m)=>l.jsx("div",{className:"l-list-item",style:{...t,padding:10,color:"#555",boxSizing:"border-box",borderBottom:"1px solid #999"},children:i.num},m)),h={title:"Component/LongListScroll",component:c,tags:["autodocs"]},r={args:{listData:n,countHeight:100,rendNum:15,render:d}};var o,e,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    listData: list,
+    countHeight: 100,
+    rendNum: 15,
+    render: renderList
+  }
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const D=["Default"];export{r as Default,D as __namedExportsOrder,h as default};
